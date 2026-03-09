b85acd7f0a778b08d84704dcff6aaee3bb86b006
lbedv <lebedyev.alex@gmail.com>
2026-03-09 20:09:56 +0100

    chore: setup app providers

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,12 @@
+/**
+ * Application theme provider wrapper.
+ * Enables dark/light mode toggling using next-themes library.
+ */
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
+} from 'next-themes'
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
