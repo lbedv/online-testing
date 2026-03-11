54d52fe8f5e478161b6319afdd24fca33e3de81d
lbedv <lebedyev.alex@gmail.com>
2026-03-11 16:02:32 +0100

    feat: add page header

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,1 +1,14 @@
-export const ProfilePage = () => <div><h1>User Profile</h1></div>;
+import { Separator } from "@/components/ui/separator";
+import { PageHeader } from "@/components/common/page-header";
+
+export function ProfilePage() {
+  return (
+    <div className="flex flex-col gap-8">
+      <PageHeader
+        title="Profile"
+        description="Manage your account information and security settings."
+      />
+      <Separator />
+    </div>
+  )
+}
