671cf725378c239e42cc61ab948f0c2f322986f5
lbedv <lebedyev.alex@gmail.com>
2026-03-22 21:12:04 +0100

    chore: configure custom variants for component states

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -65,8 +65,6 @@ function TabsTrigger({
         "group-data-[variant=line]/tabs-list:bg-transparent group-data-[variant=line]/tabs-list:data-active:bg-transparent dark:group-data-[variant=line]/tabs-list:data-active:border-transparent dark:group-data-[variant=line]/tabs-list:data-active:bg-transparent",
         "data-active:bg-background data-active:text-foreground dark:data-active:border-input dark:data-active:bg-input/30 dark:data-active:text-foreground",
         "after:absolute after:bg-foreground after:opacity-0 after:transition-opacity group-data-horizontal/tabs:after:inset-x-0 group-data-horizontal/tabs:after:bottom-[-5px] group-data-horizontal/tabs:after:h-0.5 group-data-vertical/tabs:after:inset-y-0 group-data-vertical/tabs:after:-right-1 group-data-vertical/tabs:after:w-0.5 group-data-[variant=line]/tabs-list:data-active:after:opacity-100",
-        "data-[state=active]:bg-background data-[state=active]:text-foreground",
-        "dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30",
         className
       )}
       {...props}
